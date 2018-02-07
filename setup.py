d26acd81df1410284ca8c40bc0e5d17b592a6a6d
Duncan McColl <duncmc831@gmail.com>
2018-02-07 11:44:26 -0800

    get ready to upload to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'leesl',
+  packages = ['leesl'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simple implementaion of lees L bivariate spatial association statistic.',
+  author = 'Duncan McColl',
+  author_email = 'duncmc831@gmail.com',
+  url = 'https://github.com/mrJeppard/leesl', # use the URL to the github repo
+  download_url = 'https://github.com/mrJeppard/leesl/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
